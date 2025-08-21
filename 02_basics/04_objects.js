ff829b8c07fc6ede7ad238b889e552380c5d56da
Shrasti_Agnihotri <166702473+ShrastAgnihotri@users.noreply.github.com>
2025-08-21 13:52:44 +0000

    more about objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,56 @@
+const tinderuser1 = new Object()
+// const tinderuser2 = {}
+// console.log(tinderuser2);
+// console.log(tinderuser1);
+
+// tinderuser1.name = "mani"
+// tinderuser1.id = "123"
+// tinderuser1.email = "xyzemail@.com"
+
+
+// const regularuser = {
+//     username : {
+//         name1:{
+//             firstname : "shrasti",
+//             secondname : "agnihotri"
+//         }
+//     }
+// }
+// console.log(regularuser.username.name1.firstname);
+
+
+//                          CONCATE
+
+// const obj1= {1:"a", 2:"b"}
+// const obj2 = {3:"c", 4:"d"}
+// //console.log({obj1,obj2});
+
+// const newobj = Object.assign(obj1,obj2)
+// console.log(newobj);
+// console.log();
+
+// console.log(Object.keys(tinderuser1))
+// console.log(Object.entries(tinderuser1))
+// console.log(tinderuser1.hasOwnProperty('email'))
+// console.log(tinderuser1.hasOwnProperty('age'))
+
+
+
+
+const course = {
+    coursename : "js in hindi",
+    instructor : "hitesh sir",
+    prise : "free"
+}
+
+console.log(course.instructor);
+const{instructor:i} = course
+
+console.log(i);
+
+
+
+
+
+
+
