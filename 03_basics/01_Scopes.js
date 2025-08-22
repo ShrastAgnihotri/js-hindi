3430cf2258604a9ef0396ffb2a2cadc3b789c732
Shrasti_Agnihotri <166702473+ShrastAgnihotri@users.noreply.github.com>
2025-08-22 14:46:05 +0000

    scopes in js

---
+++ 03_basics/01_Scopes.js
@@ -0,0 +1,61 @@
+// let a = 10
+// const b = 30
+// var c = 40
+
+// if(true){
+//     let a = 89
+//     const b = 45
+//     var c = 67
+
+// }
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+// function one(){
+//     const username = "mani"
+//     function two(){
+//         const website = "YT"
+//         console.log(username);
+        
+//     }
+//     //console.log(website);  ERROR
+//     //two()
+    
+// }
+// one()
+
+// if(true){
+//     const username = "mani"
+//    if(username === "mNI"){
+//         const website = "YT"
+//         console.log(username);
+        
+//     }
+//     //console.log(website);  
+    
+    
+// }
+//console.log(username);
+
+
+
+function addone(num1){
+        console.log(num1+1);
+        
+}
+
+addone(3)
+
+
+const addtwo = function(num){
+    console.log(num+2);
+    
+
+}
+
+addtwo(4)
+
+
